06622bb4804dbfa2b09e31130e4fa6f96674904a
Your Name <teonatskhadaia@yexample.com$ git config --global user.name teona>
2023-04-18 17:04:09 -0400

    created all the tables and migrated them to  pgAdmin

--- migrations/20230418204330-create-stage-event.js
+++ migrations/20230418204330-create-stage-event.js
@@ -2,38 +2,26 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('set_times', {
-      set_time_id: {
+    await queryInterface.createTable('stage_events', {
+      stage_event_id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      event_id: {
-        type: Sequelize.SMALLINT,
-        allowNull: false
-      },
+      
       stage_id: {
         type: Sequelize.SMALLINT,
         allowNull: false
       },
-      band_id: {
+      event_id: {
         type: Sequelize.SMALLINT,
         allowNull: false
-
       },
-      start_time: {
-        type: Sequelize.DATE,
-        allowNull: false
-      },
-      end_time: {
-        type: Sequelize.DATE,
-        allowNull: false
-      }
       
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('set_times');
+    await queryInterface.dropTable('stage_events');
   }
 };
--- migrations/20230418205402-create-meet-greet.js
+++ migrations/20230418205402-create-meet-greet.js
@@ -2,38 +2,33 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('set_times', {
-      set_time_id: {
+    await queryInterface.createTable('meet_greets', {
+      meet_greet_id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      event_id: {
-        type: Sequelize.SMALLINT,
+      meet_start_time: {
+        type: Sequelize.DATE,
         allowNull: false
       },
-      stage_id: {
+      meet_end_time: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      event_id: {
         type: Sequelize.SMALLINT,
         allowNull: false
       },
       band_id: {
         type: Sequelize.SMALLINT,
         allowNull: false
-
       },
-      start_time: {
-        type: Sequelize.DATE,
-        allowNull: false
-      },
-      end_time: {
-        type: Sequelize.DATE,
-        allowNull: false
-      }
       
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('set_times');
+    await queryInterface.dropTable('meet_greets');
   }
 };
