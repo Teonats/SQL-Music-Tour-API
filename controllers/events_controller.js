9f0e0f9c52840dcbe46f31d22d8ece52eab8cd49
Your Name <teonatskhadaia@yexample.com$ git config --global user.name teona>
2023-04-20 21:47:09 -0400

    Updated the Routes

--- controllers/events_controller.js
+++ controllers/events_controller.js
@@ -1,12 +1,15 @@
 const events = require('express').Router();
 const db = require('../models')
-const { Event } = db
+const { Event, Meet_Greet, Set_time, Stage, Band } = db
 const { Op } = require("sequelize");
 
 events.get('/', async (req, res)=> {
     try {
 const foundEvents = await Event.findAll({
     order: [["date", "ASC"]],
+    where: {
+        name: { [Op.like]: `%${req.query.name ? req.query.name : ''}%` }
+    }
 })
 res.status(200).json(foundEvents)
     } catch (error){
@@ -27,10 +30,35 @@ events.post('/', async (req, res) => {
     }
 })
 
-events.get('/:id', async (req, res) => {
+events.get('/:name', async (req, res) => {
     try {
         const foundEvent = await Event.findOne({
-            where: { event_id: req.params.id }
+            where: { name: req.params.name },
+            include: [
+                { 
+                    model: Meet_Greet, 
+                    as: "meet_greets", 
+                    attributes: { exclude: [ "event_id", "band_id" ] },
+                    include: {
+                         model: Band, 
+                         as: "band", 
+                    } 
+                },
+                { 
+                    model: Set_time, 
+                    as: "set_times",
+                    attributes: { exclude: [ "event_id", "stage_id", "band_id" ] },
+                    include: [
+                        { model: Band, as: "band" },
+                        { model: Stage, as: "stage" }
+                    ]
+                },
+                { 
+                    model: Stage, 
+                    as: "stages",
+                    through: { attributes: [] }
+                }
+            ]
         })
         console.log(foundEvent)
         if(!foundEvent){
